30bb5b651bbb60bb9f3e487ef2d830bbf7c49afd
JiarongGuo <158665090+JiarongGuo@users.noreply.github.com>
2024-03-30 20:09:35 +0800

    上课.bat

--- 上课.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-explorer https://appibczsojo9168.pc.xiaoe-tech.com/p/t_pc/course_pc_detail/camp_pro/course_2TrVmQ5XBTywz6ZkPgqZHDbvVRS
-explorer https://oj.tudoubiancheng.top/home
-explorer https://www.luogu.com.cn/
